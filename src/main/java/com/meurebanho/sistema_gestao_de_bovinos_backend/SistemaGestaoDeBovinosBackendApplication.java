c659f22f9651f10544f8c4697b0d40effa626e45
pedrodalagnol <joao.rossatti@edu.pucrs.br>
2025-09-15 12:37:53 -0300

    first commit

---
+++ src/main/java/com/meurebanho/sistema_gestao_de_bovinos_backend/SistemaGestaoDeBovinosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.meurebanho.sistema_gestao_de_bovinos_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestaoDeBovinosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestaoDeBovinosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meurebanho/sistema_gestao_de_bovinos_backend/SistemaGestaoDeBovinosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meurebanho.sistema_gestao_de_bovinos_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoDeBovinosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
